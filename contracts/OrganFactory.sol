f2c50e744fc241255cf719c024d1934b6be8088a
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-07-19 01:02:06 +0530

    accept organ feature added

--- contracts/OrganFactory.sol
+++ contracts/OrganFactory.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.4.23;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 /**
+
 * @title Organ Factory
 * @dev TicketFactory is a contract for managing the token ownership,
 * allowing the participants to purchase NFT with ether.
@@ -9,14 +12,19 @@ pragma solidity ^0.4.23;
 
 contract OrganFactory {
 
+    using SafeMath for uint;
+
 	struct Organ {
 		string name;
-		uint256 donorId;
+		address donorId;
 		uint256 refcode;
 		uint256 hospitalId;
-        uint256 price;
+        string blood_group;
 		bool isPurchased;
+        address purchaser_id;
 	}
+
+    // import user schema
 
 	Organ[] public organs;
 
@@ -24,26 +32,28 @@ contract OrganFactory {
 	event OrganDonated(/**/); // params to be decided
 
 	/* Mappings */
-	// Mapping from organ id to hospital
+	
+    // Mapping from organ id to hospital
     mapping (uint => address) private organToHospital;
     // Mapping from user to owned organs
     mapping (address => uint256[]) private ownedOrgans;
 
-
-    /**
-	*	@dev Donate an organ and push into the blockchain	
-		 
-    */
     function donateOrgan( 
     	string _name,
-    	uint256 _donorId,
+    	address _donorId,
     	uint256 _refcode,
 		uint256 _hospitalId,
-        uint256 price,
-		bool _isPurchased
+        string blood_group,
+		bool _isPurchased,
+        address purchase_organ
     ) public returns (uint256) {
-    	uint id = organs.push(Organ(_name,_refcode,_donorId,_hospitalId,price,_isPurchased)) - 1; // by default _isPurchased will be false
-    	//emit OrganDonated(id); // decide params
+    	uint id = organs.push(Organ(_name,_donorId,
+                                    _refcode,_hospitalId,
+                                    blood_group,
+                                    _isPurchased,purchase_organ)) - 1; // by default _isPurchased will be false and purchase_id is empty
+    	
+        //emit OrganDonated(id); // decide params
+        
         return id;
     }
 
@@ -55,19 +65,21 @@ contract OrganFactory {
     function getOrgan(uint256 _id) public view 
     returns(
     	string,
+		address,
 		uint256,
 		uint256,
-		uint256,
-        uint256,
-		bool
+        string,
+		bool,
+        address
     ) {
     	return (
     		organs[_id].name,
     		organs[_id].donorId,
     		organs[_id].refcode,
     		organs[_id].hospitalId,
-            organs[_id].price,
-    		organs[_id].isPurchased
+            organs[_id].blood_group,
+    		organs[_id].isPurchased,
+            organs[_id].purchaser_id
     	);
     }
 
@@ -79,19 +91,32 @@ contract OrganFactory {
         organs[_id].isPurchased = _isPurchased;
     }
 
-    function buyOrgan(uint256 _id,address _buyer) public payable returns (uint256) {
-        require(_id != 0);
-        require(_buyer != address(0));
+    function purchase(uint256 _id,address _buyer) public returns (bool) {
+        // check for black listed users if blacklisted return false
+        
+        organs[_id].purchaser_id = _buyer;
+        
+        // emit OrganDonated() 
+        
+        return true;        
+    } 
 
+    function purchaseOrgan(uint256 _id,address _buyer) public returns (uint256) {
+        
         Organ storage organInstance = organs[_id];
-        require(organInstance.price == msg.value);
-
+        
         // pass token from one add to another and set isPurchased->true
 
+        setIsPurchased(_id,true);
+        purchase(_id,_buyer);
         return _id;
 
     }
 
-}
+    // function getPurchasedOrgans(address _buyer) public returns (bool) {
+        
+    //     return true;
+    // }
 
+}
 
