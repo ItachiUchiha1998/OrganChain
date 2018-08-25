2c693938706ebbc85c6bd6ba59b4e01f9541ff1a
Aman Gupta <aman304gupta@gmail.com>
2018-08-25 16:33:51 +0530

    Contracts divided

---
+++ contracts/Hospital.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./OrganFactory.sol";
+
+contract Hospital {
+
+  using SafeMath for uint;
+
+  struct Hospital { // hospital structure
+    string name;
+    address hospitalId;
+  }
+
+Hospital[] public hospitals;
+
+event OrganApproved(uint256 id);
+event OrganRejected(uint256 id);
+
+
+function createHospital(string _name) public returns (bool success){ // create hospital
+  hospitals.push(Hospital(_name,msg.sender));
+  return true;
+}
+
+function acceptOrgan(uint256 _id) public returns (bool) { // approve a organ donation
+    organs[_id].isApproved = true;
+    emit OrganApproved(_id);
+    return true;
+}
+
+function rejectOrgan(uint256 _id) public returns (bool) { // reject organ donation
+    organs[_id].isApproved = false;
+    emit OrganRejected(_id);
+    return true;
+}
+
+function see_function() public view returns (uint256[]) { // see approve requests
+    return organToHospital[msg.sender];
+}
+
+}
--- contracts/OrganFactory.sol
+++ contracts/OrganFactory.sol
@@ -8,59 +8,31 @@ contract OrganFactory { // OrganFactory Contract
 	struct Organ { // organ structure
 		string name;
 		address donorId;
-		uint256 refcode;
 		address hospitalId;
 		bool isPurchased;
     address purchaser_id;
     bool isApproved;
   }
 
-  struct Hospital { // hospital structure
-    string name;
-    address hospitalId;
-    uint256[] approved;
-  }
-  Hospital[] public hospitals;
-
-function createHospital(string _name,address hospitalId) public returns (bool){ // create hospital
-  hospitals.push(Hospital(_name,hospitalId, new uint256[](0)));
-  return true;
-}
 
   Organ[] public organs;
 
 /* Events */
-	
-  event OrganCreated(address indexed donorId, uint256 indexed refcode); 
-  event OrganApproved(uint256 id); 
-  event OrganRejected(uint256 id);   
+
+  event OrganCreated(address indexed donorId, uint256 indexed refcode);
+
   event OrganDonated(uint256 id);
 
 /* Mapping */
   mapping (address => uint256[]) private organToHospital;
   mapping (address => uint256[]) private ownedOrgans;
 
-function acceptOrgan(uint256 _id) public returns (bool) { // approve a organ donation
-    organs[_id].isApproved = true;
-    emit OrganApproved(_id);
-    return true;
-}
-
-function rejectOrgan(uint256 _id) public returns (bool) { // reject organ donation
-    organs[_id].isApproved = false;
-    emit OrganRejected(_id);
-    return true;
-}
-
-function see_function(address _hospitalId) public view returns (uint256[]) { // see approve requests
-    return organToHospital[_hospitalId];
-}
 
 function donateOrgan( // Approve Organ
     	string _name,
-    	address _donorId,   
+    	address _donorId,
     	uint256 _refcode,
-  	  address _hospitalId, 
+  	  address _hospitalId,
   	  bool _isPurchased,
       address _purchaser_id,
       bool _isApproved
@@ -68,14 +40,14 @@ function donateOrgan( // Approve Organ
 
   	uint id = organs.push(Organ(_name,_donorId,
                                   _refcode,_hospitalId,
-                                  _isPurchased,_purchaser_id,_isApproved)) - 1; 
+                                  _isPurchased,_purchaser_id,_isApproved)) - 1;
 
     emit OrganCreated(_donorId , _refcode);
 
     organToHospital[_hospitalId].push(id);
-   
+
     return id;
-  
+
 }
 
 function getOrgan(uint256 _id) public view // get organ by Id
@@ -99,7 +71,7 @@ function getOrgan(uint256 _id) public view // get organ by Id
     	);
  }
 
-  function getIsPurchased(uint256 _id) public view returns (bool) { // return purchase status of organ 
+  function getIsPurchased(uint256 _id) public view returns (bool) { // return purchase status of organ
       return organs[_id].isPurchased;
   }
 
