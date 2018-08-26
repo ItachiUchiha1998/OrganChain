20e163c4b8438491b89a57df3c503f5f6a85b4b3
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-08-26 05:41:00 +0530

    deploy all 3 contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,16 @@ var OrganFactory = artifacts.require("./OrganFactory.sol");
 var Hospitals = artifacts.require("./Hospitals.sol");
 var User = artifacts.require("./User.sol")
 
+// module.exports = function(deployer) {
+//   deployer.deploy(OrganFactory,Hospitals,User);
+// };
 module.exports = function(deployer) {
-  deployer.deploy(OrganFactory);
-};
-
-module.exports = function(deployer) {
-  deployer.deploy(Hospitals);
-};
-
-module.exports = function(deployer) {
-  deployer.deploy(User);
-};
+	deployer.deploy(OrganFactory)
+	.then(function() {
+	  return deployer.deploy(Hospitals);
+	}).then(function(){
+		return deployer.deploy(User);
+	}).catch(function(err){
+		console.error(err)
+	});	
+}
